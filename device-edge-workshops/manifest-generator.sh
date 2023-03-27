6660729815de847e41ebbe0b91c37f22d54350e4
Tosin Akinosho <takinosh@redhat.com>
2023-03-27 18:11:40 -0400

    updating code

--- device-edge-workshops/manifest-generator.sh
+++ device-edge-workshops/manifest-generator.sh
@@ -7,14 +7,15 @@ if [ $# -ne 1 ]; then
 fi
 
 manifest_zip_file="$1"
+extra_vars_path="$(pwd)/device-edge-workshops/extra_vars.yml"
 
 # Encode the manifest file in base64
 base64_content=$(base64 "$manifest_zip_file")
 
 
 # Remove the existing base64_manifest line from the input YAML file
-sed -i '/^base64_manifest/d' extra_vars.yml
+sed -i '/^base64_manifest/d' $extra_vars_path
 
 # Append the new base64_manifest line with multi-line content
-echo "base64_manifest: |" | tee -a extra_vars.yml
-echo "$base64_content" | sed 's/^/  /' | tee -a extra_vars.yml
+echo "base64_manifest: |" | tee -a $extra_vars_path
+echo "$base64_content" | sed 's/^/  /' | tee -a $extra_vars_path
