fe1b39cd79939179fe510dd4239cc26460731f54
Tosin Akinosho <takinosh@redhat.com>
2023-03-27 18:15:36 -0400

    adding deploy script

---
+++ gitlab/deploy-gitlab.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+set -xe 
+if [ -f ../helper_scripts/default.env ];
+then 
+  source ../helper_scripts/default.env
+else
+  echo "default.env file does not exist"
+  exit 1
+fi
+
+cd $KCLI_SAMPLES_DIR
+
+ansiblesafe -f "${ANSIBLE_VAULT_FILE}" -o 2
+PASSWORD=$(yq eval '.admin_user_password' "${ANSIBLE_VAULT_FILE}")
+VM_NAME=gitlab-server
+IMAGE_NAME=Fedora-Cloud-Base-37-1.7.x86_64.qcow2
+DISK_SIZE=160
+sudo rm -rf kcli-profiles.yml
+if [ -f ~/.kcli/profiles.yml ]; then
+  sudo cp  ~/.kcli/profiles.yml kcli-profiles.yml
+else 
+    sudo mkdir -p ~/.kcli
+    sudo mkdir -p /root/.kcli
+fi
+if [ -d $HOME/.generated/vmfiles ]; then
+  echo "generated directory already exists"
+else
+  sudo mkdir -p  $HOME/.generated/vmfiles
+  sudo mkdir -p  /root/.generated/vmfiles
+fi
+
+sudo python3 profile_generator/profile_generator.py update_yaml gitlab gitlab/template.yaml --image ${IMAGE_NAME} --user fedora --user-password ${PASSWORD} --net-name ${NET_NAME} --disk-size ${DISK_SIZE} 
+sudo echo ${PULL_SECRET} | sudo tee pull-secret.json
+cat  kcli-profiles.yml
+ansiblesafe -f "${ANSIBLE_VAULT_FILE}" -o 1
+sudo cp kcli-profiles.yml ~/.kcli/profiles.yml
+sudo cp kcli-profiles.yml /root/.kcli/profiles.yml
+sudo rm pull-secret.json
+echo "Creating VM ${VM_NAME}"
+sudo kcli create vm -p gitlab ${VM_NAME} --wait
