b11fec855783623601c5346a3492811b11f473d5
Tosin Akinosho <takinosh@redhat.com>
2023-03-27 17:10:34 -0400

    device-edge-workshops/manifest-generator.sh

---
+++ device-edge-workshops/manifest-generator.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# Check if input file and manifest file are provided
+if [ $# -ne 1 ]; then
+    echo "Usage: $0  <manifest_zip_file>"
+    exit 1
+fi
+
+manifest_zip_file="$2"
+
+# Encode the manifest file in base64
+base64_content=$(base64 "$manifest_zip_file")
+
+# Update the base64_manifest field in the input YAML file using yq
+yq eval -i ".base64_manifest = \"$base64_content\"" extra_vars.yml
