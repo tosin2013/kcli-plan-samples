e3c6255ded38fcafa66f736ecd2991382156d8a0
Tosin Akinosho <takinosh@redhat.com>
2023-03-28 17:29:56 -0400

    testing podman

--- miniChRIS-podman-ubuntu/deploy-vm.sh
+++ miniChRIS-podman-ubuntu/deploy-vm.sh
@@ -15,8 +15,10 @@ ansiblesafe -f "${ANSIBLE_VAULT_FILE}" -o 2
 PASSWORD=$(yq eval '.admin_user_password' "${ANSIBLE_VAULT_FILE}")
 NET_NAME=qubinet
 VM_NAME=miniChRIS-podman-$(echo $RANDOM | md5sum | head -c 5; echo;)
-IMAGE_NAME=Fedora-Cloud-Base-37-1.7.x86_64.qcow2
-DISK_SIZE=50
+IMAGE_NAME=ubuntu-22.04-server-cloudimg-amd64.img
+DISK_SIZE=160
+MEMORTY=16384
+CPU_NUM=4
 sudo rm -rf kcli-profiles.yml
 if [ -f ~/.kcli/profiles.yml ]; then
   sudo cp  ~/.kcli/profiles.yml kcli-profiles.yml
@@ -24,10 +26,13 @@ else
     sudo mkdir -p ~/.kcli
     sudo mkdir -p /root/.kcli
 fi
-sudo python3 profile_generator/profile_generator.py update_yaml miniChRIS-podman-fedora miniChRIS-podman-fedora/template.yaml --image ${IMAGE_NAME} --user fedora --user-password ${PASSWORD} --net-name ${NET_NAME}  --disk-size ${DISK_SIZE} 
+sudo python3 profile_generator/profile_generator.py update_yaml miniChRIS-podman-ubuntu miniChRIS-podman-ubuntu/template.yaml \
+--image ${IMAGE_NAME} --user ubuntu --user-password ${PASSWORD} \
+--net-name ${NET_NAME}  --disk-size ${DISK_SIZE}  \
+--memory ${MEMORTY} --numcpus ${CPU_NUM}
 cat  kcli-profiles.yml
 ansiblesafe -f "${ANSIBLE_VAULT_FILE}" -o 1
 sudo cp kcli-profiles.yml ~/.kcli/profiles.yml
 sudo cp kcli-profiles.yml /root/.kcli/profiles.yml
 echo "Creating VM ${VM_NAME}"
-sudo kcli create vm -p miniChRIS-podman-fedora ${VM_NAME} --wait
+sudo kcli create vm -p miniChRIS-podman-ubuntu ${VM_NAME} --wait
